4399ca352227bc94b82c4b018eb339e0d1ce77f9
MohammedALREAI <mhamad.aa1997.aa@gmail.com>
2019-08-19 12:39:27 +0300

    wellcom to app

---
+++ src/components/App.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  const app = shallow(<App />);
+  
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+  
+  it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
+
+  describe('when clicking the `add-gift` button', () => {
+    const id = 1;
+
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
+
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id }]);
+    });
+    
+    it('adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+
+    it('creates a Gift component', () => {
+      expect(app.find('Gift').exists()).toBe(true);
+    });
+
+    describe("and the user want to delate gifts",()=>{
+
+      beforeEach(()=>{
+        app.instance().reemoveGift(id);
+      })
+    })
+
+      it("remove the gift from state ",()=>{
+
+        expect(app.state().gift).toEqual([]);
+      });
+    });
+  });
+});
